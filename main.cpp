e24f09f3579f46c1ca34898b0540cb509ad5dda7
Johel <49104482+JohelPF@users.noreply.github.com>
2020-07-13 22:20:23 -0600

    Menu con lectura de archivos

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 using namespace std;
 typedef struct _Histograma{
     int cantidad;
@@ -42,7 +44,156 @@ void RetornaHisto(){
     }
     return;
 }
+void VaciaHisto(){
+    if (primero != NULL) {
+        Histograma *recorrido = primero;
+        Histograma *temp;
+        while (true) {
+            if (recorrido->siguiente == NULL) {
+                free(recorrido);
+                primero = NULL;
+                break;
+            }
+            temp = recorrido->siguiente;
+            recorrido->siguiente = recorrido->siguiente->siguiente;
+            free(temp);
+        }
+    }
+    else{
+        return;
+    }
+}
+string lectura_archivo(){
+    ifstream archivo;
+    string texto;
+    string letra1;
+    archivo.open("input_proyecto_1.txt", ios::in);
+    if(archivo.fail()){
+        cout << "No se pudo abrir el archivo" << endl;
+        exit(1);
+    }
+
+    while(!archivo.eof()){
+        getline(archivo, letra1);
+        texto += letra1;
+    }
+    archivo.close();
+    return texto;
+}
+void procesamiento_de_estandar(int respuesta1, int respuesta2){
+    string texto;
+    cout << "Digite el texto a ser procesado" << endl;
+    cin >> noskipws >> texto;
+    if (respuesta1 == 0 && respuesta2 == 1){
+        for (int i = 0; i<texto.size(); i++){
+            if (texto[i] != ' ') {
+                InsertaHisto(texto[i]);
+            }
+        }
+        RetornaHisto();
+    }
+    else if (respuesta1 == 0 && respuesta2 == 0){
+        for (int i = 0; i<texto.size(); i++){
+            if (texto[i] != ' ') {
+                InsertaHisto(tolower(texto[i]));
+            }
+        }
+        RetornaHisto();
+    }
+    else if (respuesta1 ==1 && respuesta2 == 0){
+        for (int i = 0; i<texto.size(); i++){
+            if (texto[i] != ' ') {
+                InsertaHisto(tolower(texto[i]));
+            }
+        }
+        RetornaHisto();
+    } else{
+        for (int i = 0; i<texto.size(); i++){
+            if (texto[i] != ' ') {
+                InsertaHisto(texto[i]);
+            }
+        }
+        RetornaHisto();
+    }
+}
+void procesamiento_de_texto(int respuesta1, int respuesta2){
+    string texto = lectura_archivo();
+    if (respuesta1 == 0 && respuesta2 == 1){
+        for (int i = 0; i<texto.size(); i++){
+            if (texto[i] != ' ') {
+                InsertaHisto(texto[i]);
+            }
+        }
+        RetornaHisto();
+    }
+    else if (respuesta1 == 0 && respuesta2 == 0){
+        for (int i = 0; i<texto.size(); i++){
+            if (texto[i] != ' ') {
+                InsertaHisto(tolower(texto[i]));
+            }
+        }
+        RetornaHisto();
+    }
+    else if (respuesta1 ==1 && respuesta2 == 0){
+        for (int i = 0; i<texto.size(); i++){
+            if (texto[i] != ' ') {
+                InsertaHisto(tolower(texto[i]));
+            }
+        }
+        RetornaHisto();
+    } else{
+        for (int i = 0; i<texto.size(); i++){
+            if (texto[i] != ' ') {
+                InsertaHisto(texto[i]);
+            }
+        }
+        RetornaHisto();
+    }
+}
+void menu_opciones(int opcion){
+    int resp1;
+    int resp2;
+    if (opcion == 0){
+        cout << "Salida: '1' para estandar o '0' para archivo de texto" << endl;
+        cin >> resp1;
+        cout << "Desesa tomar en cuenta mayusculas? '1' para si y '0' para no" << endl;
+        cin >> resp2;
+        procesamiento_de_texto(resp1,resp2);
+    }
+    else{
+        cout << "Salida: '1' para estandar o '0' para archivo de texto" << endl;
+        cin >> resp1;
+        cout << "Desesa tomar en cuenta mayusculas? '1' para si y '0' para no" << endl;
+        cin >> resp2;
+        procesamiento_de_estandar(resp1,resp2);
+    }
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char respuesta;
+    do {
+        cout << "Digite la opcion que desea realizar" << endl;
+        cout << "1-Digite para procesar desde la salida estandar" << endl;
+        cout << "2-Digite para procesar desde un archivo de texto" << endl;
+        cout << "3-Digite para salir" << endl;
+        cin >> respuesta;
+        string palabra;
+        string oracion;
+        switch (respuesta) {
+            case '1':
+                VaciaHisto();
+                menu_opciones(1);;
+                break;
+            case '2':
+                VaciaHisto();
+                menu_opciones(0);
+                break;
+            case '3':
+                cout << "Saliendo gracias!" << endl;
+                exit(1);
+            default:
+                cout << "El numero digitado no corresponde a ninguna opcion" << endl;
+                break;
+        }
+    }while (respuesta != 5);
     return 0;
 }
